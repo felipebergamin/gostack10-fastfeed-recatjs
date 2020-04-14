ac2fc96e36a7d846d89db581dbd34d5f0e337be5
Felipe Bergamin <felipebergamin6@gmail.com>
2020-04-13 22:15:10 -0300

    feat: add global style

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import { Provider } from 'react-redux';
 
 import Routes from './routes';
 import store from './store';
+import GlobalStyle from './styles/global';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Routes />
+      <GlobalStyle />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
