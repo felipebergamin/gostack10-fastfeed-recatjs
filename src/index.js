fcf78c3adbf9e465c3ae00aee4837a6ea2be1de4
Felipe Bergamin <felipebergamin6@gmail.com>
2020-06-25 22:35:23 -0300

    chore: add redux persist

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
+import './config/reactotron';
 import Routes from './routes';
-import store from './store';
+import { store, persistor } from './store';
 import GlobalStyle from './styles/global';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Routes />
-      <GlobalStyle />
+      <PersistGate persistor={persistor}>
+        <Routes />
+        <GlobalStyle />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,20 @@
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import rootReducer from './rootReducer';
 
+const persistConfig = {
+  key: 'fastfeet:app',
+  storage,
+  whitelist: ['auth'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 // eslint-disable-next-line no-console
-export default createStore(rootReducer, console.tron.createEnhancer());
+export const store = createStore(
+  persistedReducer,
+  console.tron.createEnhancer()
+);
+export const persistor = persistStore(store);
