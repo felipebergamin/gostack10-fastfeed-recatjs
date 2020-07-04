f54f2ac259190dcd0625b57ac6b2f12dabacbc5e
Felipe Bergamin <felipebergamin6@gmail.com>
2020-07-04 15:34:57 -0300

    feat(recipients): recipients listing

--- src/pages/Recipients/index.js
+++ src/pages/Recipients/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { GoSearch, GoPlus } from 'react-icons/go';
+import { GoPlus, GoSearch, GoPencil, GoX } from 'react-icons/go';
+import { BsThreeDots } from 'react-icons/bs';
+import Dropdown from 'rc-dropdown';
+import Menu, { Item as MenuItem } from 'rc-menu';
 
 import { Container } from '~/styles/Container';
+import api from '~/services/api';
 
 function Recipients() {
+  const menuStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  };
+
+  const [recipientsList, setRecipientsList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await api.get('recipients');
+      setRecipientsList(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Container>
       <h3>Gerenciando Destinatários</h3>
@@ -20,6 +41,48 @@ function Recipients() {
           Cadastrar
         </Link>
       </div>
+
+      <table cellPadding="0" cellSpacing="0">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Foto</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {recipientsList.map((courier) => (
+            <tr key={String(courier.id)}>
+              <td>{courier.id}</td>
+              <td>Foto</td>
+              <td>{courier.name}</td>
+              <td>{courier.email}</td>
+              <td>
+                <Dropdown
+                  trigger={['click']}
+                  animation="slide-up"
+                  overlay={
+                    <Menu style={{ width: 90 }}>
+                      <MenuItem key="1" style={menuStyle}>
+                        <GoPencil style={{ marginRight: 10 }} />
+                        edit
+                      </MenuItem>
+                      <MenuItem key="2" style={menuStyle}>
+                        <GoX style={{ marginRight: 10 }} />
+                        delete
+                      </MenuItem>
+                    </Menu>
+                  }
+                >
+                  <BsThreeDots className="action-icon" />
+                </Dropdown>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Container>
   );
 }
