da0315cbf086fa3b0d3d4c1d107c851fcb665e28
Felipe Bergamin <felipebergamin6@gmail.com>
2020-07-05 09:48:41 -0300

    remove Dashboard component and redirect / to /login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Switch,
+  Redirect,
+  Route as RouterRoute,
+} from 'react-router-dom';
 
 import Route from './Route';
 import Login from '~/pages/Login';
-import Dashboard from '~/pages/Dashboard';
 import CouriersList from '~/pages/CouriesList';
 import AddCourier from '~/pages/AddCourier';
 import Recipients from '~/pages/Recipients';
@@ -11,12 +15,13 @@ import RecipientForm from '~/pages/RecipientForm';
 import OrdersList from '~/pages/OrdersList';
 import AddOrder from '~/pages/AddOrder';
 import Problems from '~/pages/Problems';
+import Logout from '~/pages/Logout';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Dashboard} exact isPrivate />
+        <RouterRoute path="/" render={() => <Redirect to="/login" />} exact />
         <Route path="/login" component={Login} exact />
         <Route path="/couriers" component={CouriersList} exact isPrivate />
         <Route path="/couriers/add" component={AddCourier} exact isPrivate />
@@ -42,6 +47,7 @@ export default function Routes() {
         <Route path="/orders" component={OrdersList} exact isPrivate />
         <Route path="/orders/add" component={AddOrder} exact isPrivate />
         <Route path="/delivery-problems" component={Problems} exact isPrivate />
+        <Route path="/logout" component={Logout} exact isPrivate />
       </Switch>
     </BrowserRouter>
   );
